efde754ecc52612192b15fed69d188a59d30a13c
BiliGoldenWater <2439577268@qq.com>
2020-12-15 00:00:21 +0800

    update license and add some function

--- src/DBOperator.java
+++ src/DBOperator.java
@@ -14,24 +14,46 @@ public class DBOperator{
     public Connection conn;
     public DBOperator(String fileName){
         try {
+            try {
+                Class.forName("org.sqlite.JDBC");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             conn = DriverManager.getConnection("jdbc:sqlite:"+fileName);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
     public void finalize(){
         try {
             conn.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
     public void executeCommand(String sqlCommand) throws SQLException{
-        PreparedStatement ptmt = conn.prepareStatement(sqlCommand);
-        ptmt.execute();
-        ptmt.close();
+        PreparedStatement preparedStatement = conn.prepareStatement(sqlCommand);
+        preparedStatement.execute();
+        preparedStatement.close();
+    }
+
+    public boolean executeCommand(String sqlCommand, byte handleErrByReturnBoolean){
+        if (handleErrByReturnBoolean>0){
+            PreparedStatement preparedStatement;
+            try {
+                preparedStatement = conn.prepareStatement(sqlCommand);
+                preparedStatement.execute();
+                preparedStatement.close();
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     public Map<ResultSet, PreparedStatement> executeCommand(String sqlCommand,boolean hasReturn) throws SQLException{
@@ -40,7 +62,6 @@ public class DBOperator{
         Map<ResultSet, PreparedStatement> resultSetPreparedStatementMap = new HashMap<>();
         if (hasReturn){
             rs = preparedStatement.executeQuery();
-//            System.out.println(rs.getString("name"));
         } else {
             preparedStatement.executeQuery();
         }
@@ -121,22 +142,99 @@ public class DBOperator{
                 command.append(values.get(i))
                         .append(i < values.size() - 1 ? ", " : "");
             } catch (Exception ignored){
-                command.append("\"")
-                        .append(values.get(i))
-                        .append("\"")
+                command.append(values.get(i))
                         .append(i < values.size() - 1 ? ", " : "");
             }
 
         }
         command.append(");");
 
-        try {
-            executeCommand(command.toString());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-            return false;
+        return executeCommand(command.toString(), (byte) 1);
+    }
+
+    public boolean delete(String tableName, String argName, String argEqualValue){
+        String command = "delete from " + tableName + " where " + argName + "=" + argEqualValue + ";";
+        return executeCommand(command, (byte) 1);
+    }
+
+    public boolean update(String tableName, List<String> columns, List<String> values, String argName, String argEqualValue){
+        if (columns == null || values == null || columns.size() != values.size() || columns.size() == 0) return false;
+        StringBuilder command = new StringBuilder();
+        command.append("update ")
+                .append(tableName)
+                .append(" set ");
+        for (int i = 0;i<columns.size();i++){
+            command.append(columns.get(i))
+                    .append("=")
+                    .append(values.get(i))
+                    .append(i < columns.size() - 1 ? ", " : " ");
         }
-        return true;
+        command.append("where ")
+                .append(argName)
+                .append("=")
+                .append(argEqualValue)
+                .append(";");
+
+        return executeCommand(command.toString(), (byte) 1);
+    }
+
+    public boolean delete(String tableName, String where){
+        String command = "delete from " + tableName + " where " + where + ";";
+        return executeCommand(command, (byte) 1);
+    }
+
+    public boolean update(String tableName, List<String> columns, List<String> values, String where){
+        if (columns == null || values == null || columns.size() != values.size() || columns.size() == 0) return false;
+        StringBuilder command = new StringBuilder();
+        command.append("update ")
+                .append(tableName)
+                .append(" set ");
+        for (int i = 0;i<columns.size();i++){
+            command.append(columns.get(i))
+                    .append("=")
+                    .append(values.get(i))
+                    .append(i < columns.size() - 1 ? ", " : " ");
+        }
+        command.append("where ")
+                .append(where)
+                .append(";");
+
+        return executeCommand(command.toString(), (byte) 1);
+    }
+
+    public Map<Integer, List<Object>> select(String tableName, String column, String where, int columnNum){
+        StringBuilder command = new StringBuilder();
+        Map<Integer, List<Object>> result = new HashMap<>();
+        command.append("select ")
+                .append(column)
+                .append(" from ")
+                .append(tableName)
+                .append(" where ")
+                .append(where)
+                .append(";");
+        try {
+            executeCommand(command.toString(), true).forEach((k,v)->{
+                try {
+                    for(int i=1; i<=columnNum; i++){
+                        result.put(i, new ArrayList<>());
+                    }
+
+                    while (k.next()){
+                        for(int i=1; i<=columnNum; i++){
+                            List<Object> columnArray = result.get(i);
+                            columnArray.add(k.getObject(i));
+                            result.put(i, columnArray);
+                        }
+                    }
+                    v.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     public Connection getConnection(){
